4e1b70ef7b9482894c6fb46b10e6d60919956512
WilliamBuffum <buffumw@oregonstate.edu>
2015-10-22 04:50:19 -0700

    Create simulate_roomba.cpp

---
+++ simulate_roomba.cpp
@@ -0,0 +1,16 @@
+/********************************************************
+ * Author: Billy Buffum
+ * Document: stimulate_roomba.cpp
+ * Date: 5/26/15
+ * Description: This program simulates three different
+ *               roomba vacuum cleaning algorithms and 
+ *               allows the user to test them under 
+ *               various conditions and floorplans
+ ********************************************************/
+
+#include "./simulate_roomba.h"
+//runs the program
+int main(int argc, char* argv[]){
+   simulate_roomba(argc, argv);
+   return 0;
+}
